e89e546dc2863d5dcdba3c8083bcf4405e54a199
Bklzn <adrian_bon@wp.pl>
2025-07-15 13:39:36 +0200

    backend - docker preparation

--- backend/RoomFunds/urls.py
+++ backend/RoomFunds/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
@@ -27,4 +29,4 @@ urlpatterns = [
     path("", include("core.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
