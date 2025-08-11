17aec0c788db24264368a715d891467504c00c29
Bklzn <adrian_bon@wp.pl>
2025-08-11 13:45:58 +0200

    gh-pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react(), tailwindcss()],
+    base: "/RoomFunds",
     define: {
       BASE_URL: JSON.stringify(env.baseUrl),
     },
